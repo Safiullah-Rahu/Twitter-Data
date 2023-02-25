0281db4df98a4e576babb86a33f17744aa6250b6
Safiullah Rahu <safiullahrahu1555@gmail.com>
2023-02-26 04:46:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,42 +2,21 @@ import streamlit as st
 import pandas as pd
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import os
+import re
+import random
+import time
+import hydralit_components as hc
+import pandas as pd
+
 
 # Set layout to wide
 st.set_page_config(layout="wide")
 
-# Define the app description
-st.write("""
-# Twitter Data Analysis App
-
-This app allows you to analyze Twitter data from an Excel file. This app will be further developed to preprocess this tweets dataset for Fine Tuning Large Language Models in future.
-
-""")
-
-st.subheader("Upload xlsx File of Tweets")
-st.write("""Go to 'https://www.vicinitas.io/' and download the tweets of your desired Twitter handle by writing username of the twitter account and then an xlsx file will be downloaded. Upload that file below to Analyze Tweet Data.""")
-# Define a function to allow the user to upload a file
-# def file_upload():
-#     uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
-#     if uploaded_file is not None:
-#         df = pd.read_excel(uploaded_file)
-#         return df
-#     else:
-#         return None
-def file_upload():
-    uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
-    if uploaded_file is not None:
-        return uploaded_file
-# Allow the user to upload a file and store it in a variable
-file = file_upload()
-# 'elonmusk_user_tweets.xlsx'
-# If a file was uploaded, display its data using Streamlit
-if file is not None:
-    # Use pandas to read in the Excel file
-    df = pd.read_excel(file , usecols=['Tweet Id', 'Text', 'Name', 'Screen Name', 'UTC', 'Created At', 'Favorites', 'Retweets', 'Language', 'Client', 'Tweet Type'])
+# Create a function to analyze the dataset
+def analyze_data(df):
+    st.write("Analysis Results:")
     # Display some basic information about the data
-    st.sidebar.write("## About")
-    st.sidebar.write("This app allows you to analyze Twitter data from an Excel file.This app will be further developed to preprocess this tweets dataset for Fine Tuning Large Language Models in future.")
     st.sidebar.write(f"Number of tweets: {len(df)}")
     st.sidebar.write(f"Languages present: {df['Language'].value_counts().idxmax()}")
     Name = df["Name"][0]
@@ -98,4 +77,129 @@ if file is not None:
     ax.imshow(wordcloud, interpolation="bilinear")
     plt.axis("off")
     st.pyplot(fig)
+    # Do analysis here and display results
+    # ...
 
+# Create functions to preprocess the dataset
+handles_processed = []
+ALLOW_NEW_LINES = False
+EPOCHS = 4
+
+def fix_text(text):
+        text = text.replace('&amp;', '&')
+        text = text.replace('&lt;', '<')
+        text = text.replace('&gt;', '>')
+        return text
+    
+def clean_tweet(tweet, allow_new_lines = ALLOW_NEW_LINES):
+        bad_start = ['http:', 'https:']
+        for w in bad_start:
+            tweet = re.sub(f" {w}\\S+", "", tweet)      # removes white space before url
+            tweet = re.sub(f"{w}\\S+ ", "", tweet)      # in case a tweet starts with a url
+            tweet = re.sub(f"\n{w}\\S+ ", "", tweet)    # in case the url is on a new line
+            tweet = re.sub(f"\n{w}\\S+", "", tweet)     # in case the url is alone on a new line
+            tweet = re.sub(f"{w}\\S+", "", tweet)       # any other case?
+        tweet = re.sub(' +', ' ', tweet)                # replace multiple spaces with one space
+        if not allow_new_lines:                         # TODO: predictions seem better without new lines
+            tweet = ' '.join(tweet.split())
+        return tweet.strip()
+
+def boring_tweet(tweet):
+        "Check if this is a boring tweet"
+        boring_stuff = ['http', '@', '#']
+        not_boring_words = len([None for w in tweet.split() if all(bs not in w.lower() for bs in boring_stuff)])
+        return not_boring_words < 3
+
+def preprocess_data(df):
+    st.write("Preprocessing Dataset:")
+    # Do preprocessing here and display results
+    res = {}
+    res["tweets"] = df["Text"].tolist()
+    res['n_tweets'] = len(res["tweets"])
+    res['n_RT'] = df.Retweets.sum()
+    all_tweets = res["tweets"]
+    cool_tweets = []
+    handles_processed = []
+    raw_tweets = []
+    user_names = []
+    n_tweets_dl = []
+    n_retweets = []
+    n_short_tweets = []
+    n_tweets_kept = []
+    i = 0
+    raw_tweets.append(all_tweets)
+    curated_tweets = [fix_text(tweet) for tweet in all_tweets]
+    # create dataset
+    clean_tweets = [clean_tweet(tweet) for tweet in curated_tweets]
+    cool_tweets.append([tweet for tweet in clean_tweets if not boring_tweet(tweet)])
+    # save count
+    n_tweets_dl.append(str(res['n_tweets']))
+    n_retweets.append(str(res['n_RT']))
+    n_short_tweets.append(str(len(all_tweets) - len(cool_tweets[-1])))
+    n_tweets_kept.append(str(len(cool_tweets[-1])))
+    
+    if len('<|endoftext|>'.join(cool_tweets[-1])) < 6000:
+        # need about 4000 chars for one data sample (but depends on spaces, etc)
+        raise ValueError(f"Error: this user does not have enough tweets to train a Neural Network\n{res['n_tweets']} tweets downloaded, including {res['n_RT']} RT's and {len(all_tweets) - len(cool_tweets)} boring tweets... only {len(cool_tweets)} tweets kept!")
+    if len('<|endoftext|>'.join(cool_tweets[-1])) < 40000:
+        st.write('\n<b>Warning: this user does not have many tweets which may impact the results of the Neural Network</b>\n')
+    st.write(f"\n{n_tweets_dl[-1]} tweets detected, including {n_retweets[-1]} RT's and {n_short_tweets[-1]} short tweets... keeping {n_tweets_kept[-1]} tweets\n\n\n")
+    # create a file based on multiple epochs with tweets mixed up
+    seed_data = random.randint(0,2**32-1)
+    dataRandom = random.Random(seed_data)
+    total_text = '<|endoftext|>'
+    all_handle_tweets = []
+    epoch_len = max(len(''.join(cool_tweet)) for cool_tweet in cool_tweets)
+    for _ in range(EPOCHS):
+        for cool_tweet in cool_tweets:
+            dataRandom.shuffle(cool_tweet)
+            current_tweet = cool_tweet
+            current_len = len(''.join(current_tweet))
+            while current_len < epoch_len:
+                for t in cool_tweet:
+                    current_tweet.append(t)
+                    current_len += len(t)
+                    if current_len >= epoch_len: break
+            dataRandom.shuffle(current_tweet)
+            all_handle_tweets.extend(current_tweet)
+            total_text += '<|endoftext|>'.join(all_handle_tweets) + '<|endoftext|>'
+
+    st.write('\nCreating dataset...')
+    # for 4 replications of the same loader (index=2) from the standard loader group
+    with hc.HyLoader('Processing',hc.Loaders.standard_loaders,index=0):
+        time.sleep(8)
+    #if st.button("Save Processed Dataset"):
+    with open(f"data_{df['Screen Name'][0]}_train.txt", 'w', encoding='utf-8') as f:
+        f.write(total_text)
+    st.write('\n🎉 Dataset created successfully!')
+    st.write('\n🎉 Dataset saved successfully!')
+    
+    
+def long_running_process():
+    for i in range(10):
+        time.sleep(0.5)
+        st.experimental_rerun()
+        
+# Set up the Streamlit app
+st.title("Twitter Data Analysis and Preprocessing App")
+st.write("This app allows you to analyze Twitter data from an Excel file and preprocess tweets dataset for Fine Tuning Large Language Models.")
+st.write("Upload a dataset in CSV format:")
+st.sidebar.title("Features")
+# Allow user to upload a dataset
+uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
+
+# If a dataset has been uploaded, display a button to analyze it
+if uploaded_file is not None:
+    # Read the dataset into a Pandas dataframe
+    df = pd.read_excel(uploaded_file)
+    # Display the first few rows of the dataset
+    if st.button("View Dataset"):
+        st.write("Dataset Preview:")
+        st.write(df.head())
+
+    # Display buttons for analysis and preprocessing
+    if st.sidebar.button("Analyze Data"):
+        analyze_data(df)
+
+    if st.sidebar.button("Preprocess Data"):
+        preprocess_data(df)
